d1d0426a5bddffe1326330044610ea693a61da5f
Marco Serrano <marcoTheWebDev@gmail.com>
2021-09-11 00:05:40 -0500

    Changed line endings from CRLF to LF.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,13 +3,13 @@ import Web3 from 'web3';
 let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-    window.ethereum.request({ method: 'eth_requestAccounts' });
-    web3 = new Web3(window.ethereum);
+  window.ethereum.request({ method: 'eth_requestAccounts' });
+  web3 = new Web3(window.ethereum);
 } else {
-    const provider = new Web3.providers.HttpProvider(
-        process.env.INFURA_ENDPOINT
-    );
-    web3 = new Web3(provider);
+  const provider = new Web3.providers.HttpProvider(
+    process.env.INFURA_ENDPOINT,
+  );
+  web3 = new Web3(provider);
 }
 
 export default web3;
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { beforeEach, describe, it } = require('mocha');
+
 const web3 = new Web3(ganache.provider());
-const compiledBillboard = require('../ethereum/build/InternetBillboard.json')
+const compiledBillboard = require('../ethereum/build/InternetBillboard.json');
 
 let accounts;
 let inbox;
@@ -10,33 +12,31 @@ let defaultMessage;
 let newMessage;
 
 beforeEach(async () => {
-    // Get a list of all accounts
-    accounts = await web3.eth.getAccounts();
-    defaultMessage = 'Hello world!';
-    newMessage = 'Goodbye world!';
+  accounts = await web3.eth.getAccounts();
+  defaultMessage = 'Hello world!';
+  newMessage = 'Goodbye world!';
 
-    // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(compiledBillboard.abi))
-        .deploy({
-            data: compiledBillboard.evm.bytecode.object,
-            arguments: [defaultMessage]
-        })
-        .send({ from: accounts[0], gas: '1000000' });
+  inbox = await new web3.eth.Contract(compiledBillboard.abi)
+    .deploy({
+      data: compiledBillboard.evm.bytecode.object,
+      arguments: [defaultMessage],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {
-        assert.ok(inbox.options.address)
-    });
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
 
-    it('has a default message', async () => {
-        const message = await inbox.methods.message().call();
-        assert.strictEqual(defaultMessage, message);
-    });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(defaultMessage, message);
+  });
 
-    it('can set message', async () => {
-        await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
-        const message = await inbox.methods.message().call();
-        assert.strictEqual(newMessage, message);
-    });
+  it('can set message', async () => {
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(newMessage, message);
+  });
 });
